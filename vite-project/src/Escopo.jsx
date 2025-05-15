fe4427bea9c7a58731769df0de2c446c426c2cd8
Ariane valeria <153088012+arianevaleira@users.noreply.github.com>
2025-05-15 16:48:20 -0300

    Add files via upload

---
+++ vite-project/src/Escopo.jsx
@@ -0,0 +1,17 @@
+function Escopo(){
+    const label='clique aqui'
+    return(
+        <>
+          <div>
+             <h2>texto de componentes</h2>
+             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum quas, minima voluptate veniam perferendis sit? Ducimus obcaecati nesciunt qui veniam aliquid eaque expedita quas unde a at? Quasi, voluptas dolorum?</p>
+          </div>
+          <div>
+             <button>{label}</button>
+          </div>
+        </>
+    )
+}
+
+
+export default Escopo //Todo componente deve ter um retorno
