b8869a7295a2f07ead7b92366634884249cbc921
Ariane valeira <costa.valeria@escolar.ifrn.edu.br>
2025-05-15 17:40:54 -0300

    oi

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import Escopo from './componentes/Escopo'
+import Botao from './componentes/Botao'
+import EscopoReduz from './componentes/EscopoReduz'
+import Info_1v from './componentes/Info_1v'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+     <>
+      {/* <Escopo/>
+      <Escopo/>
+      <Escopo/>
+      <Botao/>
+      <EscopoReduz/> */}
+      <Info_1v/>
+     </>
+  )
+}
+
+
+
+
+export default App
