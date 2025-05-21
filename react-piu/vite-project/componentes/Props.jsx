304f4244c8bc957c99f454957c52da2987f0cc1a
Ariane valeria <153088012+arianevaleira@users.noreply.github.com>
2025-05-21 13:32:56 -0300

    Add files via upload

---
+++ react-piu/vite-project/componentes/Props.jsx
@@ -0,0 +1,12 @@
+export default function Props ({nome, newname}){
+
+    return(
+        <>
+            <div>
+                <h2>Teste props</h2>
+                <p>O nome {nome} foi enviado via props</p>
+                <p>{newname}</p>
+            </div>
+        </>
+    )
+}
