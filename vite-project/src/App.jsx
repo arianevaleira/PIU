10655fac68270b17a9e57563f016d536aa99cc0e
Ariane valeira <costa.valeria@escolar.ifrn.edu.br>
2025-05-22 10:01:32 -0300

    oi

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,6 +3,11 @@ import Escopo from './componentes/Escopo'
 import Botao from './componentes/Botao'
 import EscopoReduz from './componentes/EscopoReduz'
 import Info_1v from './componentes/Info_1v'
+import ChangeBackground from './componentes/ChangeBackground'
+import ChangeValue from './componentes/ChangeValue'
+import Props from './componentes/Props'
+import Children from './componentes/Children'
+import Card from './componentes/Card'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -14,7 +19,15 @@ function App() {
       <Escopo/>
       <Botao/>
       <EscopoReduz/> */}
-      <Info_1v/>
+      {/* <Info_1v/>
+      <Props nome={"Um nome"} newname={"novo"} matricula={20221101110024}/> */}
+      <Children>
+        <Card/>
+      </Children>
+      <br />
+      <Children>
+        <Card/>
+      </Children>
      </>
   )
 }
---
+++ vite-project/src/componentes/Children.jsx
@@ -0,0 +1,9 @@
+//recebimen
+export default function Children ({children}){
+
+    return(
+        <div style={{border: "4px solid pink", padding:"20px"}}>
+            {children}
+        </div>
+    )
+}
