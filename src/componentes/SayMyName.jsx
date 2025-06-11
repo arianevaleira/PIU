050a474597c2c72fd7b6645130454bf3c117e5cd
arianevaleira <costa.valeria@escolar.ifrn.edu.br>
2025-06-11 09:14:19 -0300

    Atualizando Valores

---
+++ src/componentes/SayMyName.jsx
@@ -0,0 +1,9 @@
+function SayMyName (props){
+    return (
+        <div>
+            <p>usando a proprieda props para falar com: {props.name}</p>
+        </div>
+    )
+}
+
+export default SayMyName
