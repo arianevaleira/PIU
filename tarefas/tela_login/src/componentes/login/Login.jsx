c1a5e82eb334749b02377b8302d5b14de25264e1
arianevaleira <costa.valeria@escolar.ifrn.edu.br>
2025-08-05 17:06:01 -0300

    Tela De Login

--- tarefas/tela_login/src/componentes/login/Login.jsx
+++ tarefas/tela_login/src/componentes/login/Login.jsx
@@ -1,17 +1,30 @@
+import { useState} from 'react'
 import './Login.css'
 
 function Login (){
+    const [mostrar, setMostrar] = useState(false)  
+    const [email, setEmail] = useState('')  
+    const [senha, setSenha] = useState('')
+
+    function cadastrarUsuario(e){
+        e.preventDefault()
+        console.log('Usuário cadastrado:', { email, senha })
+        setMostrar(true)
+    }
+
+
     return(
        <div className="Conteiner">
-           <form >
+           <form onSubmit={cadastrarUsuario}>
                 <h1>Acesse o Sistema</h1>
                 <div>
-                    <input type="email" placeholder="E-mail" />
+                    <input type="email" id='email' placeholder="E-mail" onChange={(e) =>  setEmail(e.target.value)} />
                 </div>
                 <div>
-                    <input type="password" placeholder="Senha" />
+                    <input type="password" id='senha' placeholder="Senha" name='senha' onChange={(e) => setSenha(e.target.value)} />
                 </div>
                 <button>Entrar</button>
+                {mostrar && <p>Usuario: {email} | Senha: {senha}</p>}
            </form>
        </div>
     )
